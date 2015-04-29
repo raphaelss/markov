dae3be15313bd4528985ec7cb26011a719f837e2
Raphael Santos <contact@raphaelss.com>
2015-04-29 18:59:47 -0300

    move to c++14 and improve example application

--- main.cpp
+++ main.cpp
@@ -1,16 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include <random>
+#include <string>
 #include "markov.hpp"
 
-int main() {
+void read_stream(std::istream &in,
+    markov::markov_chain_nth<std::string> &chain);
+
+int main(int argc, char **argv) {
+  if (argc < 4) {
+    std::cerr << "usage: markov order n file1 [file2 file3 ...fileN]\n";
+    return 1;
+  }
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::vector<int> seq {0,1,2,0,3,4,5,6,7,8,0,1,0,3,4,5,6,7,2,0};
-  markov::markov_chain_nth<int> chain = markov::markov_create<int>(2);
-  markov::read_seq(chain, seq.begin(), seq.end());
-  std::vector <int> vec {0, 3};
-  markov::generate_seq(gen, chain, vec, 10, [](int x) {
-    std::cout << x << std::endl;
+  auto chain = markov::markov_create<std::string>(std::stoi(argv[1]));
+  for (int i = 3; i < argc; ++i) {
+    std::ifstream fin(argv[i]);
+    read_stream(fin, chain);
+  }
+  std::vector<std::string> vec(chain.order(), "");
+  markov::generate_seq(gen, chain, vec, std::stoi(argv[2]), [](auto &x) {
+    std::cout << x << " ";
   });
+  std::cout << std::endl;
   return 0;
+}
+
+void read_stream(std::istream &in,
+    markov::markov_chain_nth<std::string> &chain) {
+  std::vector<std::string> seq(chain.order(), "");
+  std::string tmp;
+  while (in >> tmp) {
+    seq.push_back(tmp);
+    chain.add_from_seq(seq.begin());
+    seq.erase(seq.begin());
+  }
 }

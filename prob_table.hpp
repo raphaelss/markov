90c644bae7e736b52052619bfb2acf886ee4fe4e
Raphael Santos <contact@raphaelss.com>
2015-08-31 17:31:39 -0300

    fix prob_table.hpp

--- prob_table.hpp
+++ prob_table.hpp
@@ -1,19 +1,19 @@
-/**
-* Copyright (C) 2015  Raphael Sousa Santos, http://www.raphaelss.com
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-**/
+/*
+ * Copyright (C) 2015  Raphael Sousa Santos, http://www.raphaelss.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef PROB_TABLE_HPP_MARKOVMODEL_INCLUDED
 #define PROB_TABLE_HPP_MARKOVMODEL_INCLUDED
@@ -25,136 +25,87 @@ namespace markov {
 template <class T>
 class prob_table {
 public:
-  prob_table();
+  prob_table(): _table(), _sum(0) {}
 
-  double probability(const T &x) const;
+  double probability(const T &x) const {
+    try {
+      return _table.at(x);
+    } catch (std::out_of_range &e) {
+      return 0;
+    }
+  }
 
-  double get(const T &x) const;
+  double get(const T &x) const {
+    return _table.at(x);
+  }
 
-  double prob_sum() const;
+  double prob_sum() const {
+    return _sum;
+  }
 
   template <class Tt>
-  void add(Tt &&x, double prob=1);
+  void (Tt &&x, double prob) {
+    _table[x] += prob;
+    _sum += prob;
+  }
 
   template <class Tt>
-  void set(Tt &&x, double prob);
+  void set(Tt &&x, double prob) {
+    _sum = _sum - probability(x) + prob;
+    _table[x] = prob;
+  }
 
-  bool has(const T &x) const;
+  bool has(const T &x) const {
+    return _table.find(x) != _table.end();
+  }
 
-  bool empty();
+  bool empty() const {
+    return _table.empty();
+  }
 
-  void clear();
+  void clear() {
+    _table.clear();
+    _sum = 0;
+  }
 
   template <class RndGen>
-  T choose(RndGen &gen) const;
+  T choose(RndGen &gen) const {
+    std::uniform_real_distribution<double> dist(0, _sum);
+    auto it = _table.begin();
+    for (double r = dist(gen) - it->second; r > 0; ++it, r -= it->second);
+    return it->first;
+  }
 
-  void normalize(double total=1);
+  void normalize(double total=1) {
+    for (auto &pair : _table) {
+      pair.second /= _sum;
+    }
+    _sum = 1;
+  }
 
-  void cbegin() const;
-  void cend() const;
+  void cbegin() const {
+    return _table.cbegin();
+  }
 
-  void merge(const prob_table<T> &other);
+  void cend() const {
+    return _table.cend();
+  }
 
-  void move_merge(prob_table<T> &&other);
+  void merge(const prob_table<T> &other) {
+    for (const auto &pair : other._table) {
+      _table[pair->first] += other._table[pair->second];
+    }
+  }
+
+  void move_merge(prob_table<T> &&other) {
+    for (auto &pair : other._table) {
+      _table[std::move(pair->first)] += other._table[pair->second];
+    }
+  }
 
 private:
   std::unordered_map<T, double> _table;
   double _sum;
 };
-
-template <class T>
-prob_table<T>::prob_table(): _table(), _sum(0) {}
-
-template <class T>
-double prob_table<T>::probability(const T &x) const {
-  try {
-    return _table.at(x);
-  } catch (std::out_of_range &e) {
-    return 0;
-  }
-}
-
-template <class T>
-double prob_table<T>::get(const T &x) const {
-  return _table.at(x);
-}
-
-template <class T>
-double prob_table<T>::prob_sum() const {
-  return _sum;
-}
-
-template <class T>
-template <class Tt>
-void prob_table<T>::add(Tt &&x, double prob) {
-  _table[x] += prob;
-  _sum += prob;
-}
-
-template <class T>
-template <class Tt>
-void prob_table<T>::set(Tt &&x, double prob) {
-  _sum = _sum - probability(x) + prob;
-  _table[x] = prob;
-}
-
-template <class T>
-bool prob_table<T>::has(const T &x) const {
-  return _table.find(x) != _table.end();
-}
-
-template <class T>
-bool prob_table<T>::empty() {
-  return _table.empty();
-}
-
-template <class T>
-void prob_table<T>::clear() {
-  _table.clear();
-  _sum = 0;
-}
-
-template <class T>
-template <class RndGen>
-T prob_table<T>::choose(RndGen &gen) const {
-  std::uniform_real_distribution<double> dist(0, _sum);
-  auto iter = _table.begin();
-  for (double r = dist(gen) - iter->second; r > 0; ++iter, r -= iter->second);
-    return iter->first;
-}
-
-template <class T>
-void prob_table<T>::normalize(double total) {
-  for (auto &pair : _table) {
-    pair.second /= _sum;
-  }
-  _sum = 1;
-}
-
-template <class T>
-void prob_table<T>::cbegin() const {
-  return _table.cbegin();
-}
-
-template <class T>
-void prob_table<T>::cend() const {
-  return _table.cend();
-}
-
-template <class T>
-void prob_table<T>::merge(const prob_table<T> &other) {
-  for (auto &pair : other._table) {
-    _table[pair->first] += other._table[pair->second];
-  }
-}
-
-template <class T>
-void prob_table<T>::move_merge(prob_table<T> &&other) {
-  for (auto &pair : other._table) {
-    _table[std::move(pair->first)] += other._table[pair->second];
-  }
-}
-
-}
 
 #endif
